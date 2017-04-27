a1fee2ee4537e984bc4ac6d55a606c06042161b3
Lewi Samuel <lewiribeiro@cjr.org.br>
2017-04-27 07:49:09 -0300

    Adicionar Javascript

---
+++ app.js
@@ -0,0 +1,21 @@
+function andando(){
+	console.log("andando.......")
+}
+
+var carro = {
+	"cor":"vermelho",
+	"um":"1",
+	"dois":"2",
+	"funcao": andando,
+}
+for (var i = 1; i <= 100; i++) {
+	console.log("Você é doente cara");
+	console.log(i);
+	
+}
+
+var pilha = {
+	"push":pushar,
+	"pop":popar,
+	"stack":stackar
+}
